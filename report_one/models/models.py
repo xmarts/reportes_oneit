a2eb1f16e9deedbf7d2301411f4ed9898fe443c1
soto <Hz95EuA5A>
2019-06-04 16:06:06 -0500

    actualizacion
    campo amount text

--- report_one/models/models.py
+++ report_one/models/models.py
@@ -25,7 +25,15 @@ class amount_to_texts(models.Model):
 	def _get_amount_to_text(self):
 		self.amount_to_text = amount_to_text.get_amount_to_text(self, self.amount_total)
 
-
+class amount_to_texts(models.Model):
+	_inherit='account.invoice'
+	amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True,
+								help='Amount of the invoice in letter')
+	@api.one
+	@api.depends('amount_total')
+	def _get_amount_to_text(self):
+		self.amount_to_text = amount_to_text.get_amount_to_text(self, self.amount_total)
+	
 	
 class campos_purchase(models.Model):
 	_inherit='purchase.order.line'
