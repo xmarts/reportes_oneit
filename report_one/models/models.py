04ad8d7f955b58c4f5f135df455e6e8b76769c1e
soto <victor.alonso@xmarts.com>
2019-08-26 11:33:09 -0500

    actualizacion
    -.-

--- report_one/models/models.py
+++ report_one/models/models.py
@@ -75,3 +75,16 @@ class img(models.Model):
 									attachment=True,
 									help="This field holds the image used for" +
 									"the badge, limited to 256x256")
+
+class pagos_pagos(models.Model):
+    _inherit = 'account.payment'
+
+    @api.model
+    def l10n_mx_edi_get_pago_etree(self, cfdi):
+        if not hasattr(cfdi, 'Complemento'):
+            return None
+        attribute = 'pago10:Pagos[1]'
+        namespace = {'pago10': 'http://www.sat.gob.mx/Pagos'}
+        node = cfdi.Complemento.xpath(attribute, namespaces=namespace)
+        return node[0] if node else None
+
